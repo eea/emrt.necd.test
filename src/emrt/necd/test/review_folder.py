1b3dc4bd9b16020d0aa96d1667077993a62ec369
David Batranu <david.batranu@eaudeweb.ro>
2017-02-17 19:31:49 +0200

    [refs #82126] Add test for observation add.

--- src/emrt/necd/test/review_folder.py
+++ src/emrt/necd/test/review_folder.py
@@ -1,36 +1,71 @@
 import unittest
 
-from emrt.necd.test.util import runas
+import emrt.necd.test.util as util
 from edw.seleniumtesting.common import BrowserTestCase
-from selenium.webdriver.common.keys import Keys
+
 
 def suite(browser, base_url, extra_args):
-
-    # setup a new suite
-    suite = unittest.TestSuite()
+    """ Call on review folder url providing user mapping and user accounts.
+    `` seleniumtesting http://localhost/Plone/2017 \
+        -ea roles sectorexpert acc_sectorexpert \
+        -ea users acc_sectorexpert acc_sectorexpert_pwd
+    ``
+    """
+    test_suite = unittest.TestSuite()
 
     for name in ReviewFolderTestCase.my_tests():
         testcase = ReviewFolderTestCase(name, browser, base_url, extra_args)
-        suite.addTest(testcase)
-    
-    return suite
+        test_suite.addTest(testcase)
+
+    return test_suite
+
 
 class ReviewFolderTestCase(BrowserTestCase):
 
     def setUp(self):
         self.browser.get(self.url)
-    
-    @runas("admin")
-    def test_review_folder(self):
-        """Test 'Save Observation' button exists
+
+    @util.runas('sectorexpert')
+    def test_00_review_folder(self):
+        """ Test 'Save Observation' button exists
         """
         self.browser.get(self.url)
 
-        new_obs = self.browser.find_element_by_link_text("New observation")
+        new_obs = util.find_link(self.browser, "New observation")
         self.assertEqual("New observation", new_obs.text)
-        
-        #import pdb; pdb.set_trace()
-        new_obs.click()
-        
-        save_obs = self.browser.find_element_by_name("form.buttons.save")
-        self.assertEqual("Save Observation", save_obs.get_attribute('value'))
+
+    @util.runas('sectorexpert')
+    def test_01_add_observation(self):
+        """ Test sectorexpert can add an observation.
+        """
+        self.browser.get(self.url)
+
+        # click new observation button
+        util.find_link(self.browser, "New observation").click()
+
+        # fill in form fields
+        util.find_name(self.browser, 'form.widgets.text').send_keys('Test observation')
+        util.find_name(self.browser, 'form.widgets.year').send_keys('2017')
+        util.find_name(self.browser, 'form.widgets.pollutants:list').click()
+        util.find_name(self.browser, 'form.widgets.parameter:list').click()
+
+        # submit form
+        util.find_name(self.browser, "form.buttons.save").click()
+
+        # check saved information
+        metadata_div = util.find_css(self.browser, '.esdDiv').text
+
+        self.assertTrue('Austria' in metadata_div)
+        self.assertTrue('1A1a' in metadata_div)
+        self.assertTrue('NOx' in metadata_div)
+        self.assertTrue('2017' in metadata_div)
+
+        # expand collapsed elements so the next assertion works
+        for elem in self.browser.find_elements_by_css_selector('.collapsed'):
+            elem.click()
+
+        # check saved text exists
+        self.assertTrue(
+            'Test observation' in
+            util.find_css(self.browser, '.esdDiv').text
+        )
--- src/emrt/necd/test/util.py
+++ src/emrt/necd/test/util.py
@@ -1,16 +1,44 @@
 from selenium.webdriver.common.keys import Keys
 
-def runas(username):
+
+def runas(role='', user='', pwd_from='users', usr_from='roles'):
     def decorator(func):
         def wrapped(self):
+            # extract credentials
+            if role:
+                username = self.extra_args[usr_from][role]
+            elif user:
+                username = user
+
+            password = self.extra_args[pwd_from][username]
+
+            # open login
             self.browser.get(self.url + '/login')
+
+            # get form fields
             login_name = self.browser.find_element_by_id("__ac_name")
             passwd = self.browser.find_element_by_id("__ac_password")
-            
-            login_name.send_keys(username)
-            passwd.send_keys(self.extra_args["users"][username])
 
+            # fill in form
+            login_name.send_keys(username)
+            passwd.send_keys(password)
+
+            # submit
             self.browser.find_element_by_name("submit").send_keys(Keys.RETURN)
+
+            # call wrapped function
             return func(self)
         return wrapped
     return decorator
+
+
+def find_css(browser, selector):
+    return browser.find_element_by_css_selector(selector)
+
+
+def find_name(browser, name):
+    return browser.find_element_by_name(name)
+
+
+def find_link(browser, text):
+    return browser.find_element_by_link_text(text)
