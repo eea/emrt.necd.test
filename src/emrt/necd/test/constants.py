ef3e1837a16d82936431e0375e17683c0999437e
Irina Botez <irina.botez@eaudeweb.ro>
2018-04-25 12:36:40 +0300

    New module for registering buttons that appear in a certain state of the Question

---
+++ src/emrt/necd/test/constants.py
@@ -0,0 +1,28 @@
+SE_DRAFT = [
+    'Edit question', 'Upload file', 'Delete Question', 'Go to Conclusions',
+    'Request Comments', 'Send Question for Approval', 'Edit Key Flags'
+]
+
+SE_DRAFT_OBSERVATION = [
+    'Add question', 'Edit observation',
+    'Delete observation', 'Go to conclusions'
+]
+
+SE_COUNTERPART_COMMENTS = [
+    'Select new Counterparts', 'Close Comments', 'Edit Key Flags'
+]
+
+LR_PENDING = SE_DRAFTED = ['Recall Question', 'Edit Key Flags']
+
+LR_DRAFTED = [
+    'Edit question', 'Upload file', 'Approve question and send',
+    'Go to Conclusions', 'Ask SE to redraft', 'Edit Key Flags'
+]
+
+LR_CLOSED = ['Edit conclusion', 'Upload file', 'Edit Key Flags']
+
+MSA_PENDING_ANSWER_DRAFTING = [
+    'Edit answer', 'Upload file', 'Submit Answer', 'Request Comments'
+]
+
+MSA_PENDING = ['Create answer', 'Request input for answer']
