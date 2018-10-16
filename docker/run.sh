a4a2a47dda57818bdf1646957e403b71e94bae98
Irina Botez <irina.botez@eaudeweb.ro>
2018-10-16 14:27:40 +0300

    Updated selenium command to set window size

--- docker/run.sh
+++ docker/run.sh
@@ -16,12 +16,11 @@ TESTS=(
     remove_test_site
 )
 
-COMMAND_RUN="/home/selenium/.local/bin/seleniumtesting -v -B chrome -P /usr/bin/chromedriver -A='--headless' http://plone:8080/Workflow_test"
+COMMAND_RUN="/home/selenium/.local/bin/seleniumtesting -v -B chrome -P /usr/bin/chromedriver -A='--headless' -sw 3840 -sh 2160 http://plone:8080/Workflow_test"
 COMMAND_ARGS="-ea ldap_credentials $LDAP_USER $LDAP_PASSWORD -ea zope_user $ZOPE_USER $ZOPE_PASSWORD -ea roles sectorexpert $EXPERT_USER -ea users $EXPERT_USER $EXPERT_PASSWORD -ea roles leadreviewer $REVIEW_USER -ea users $REVIEW_USER $REVIEW_PASSWORD -ea roles msauthority $AUTHORITY_USER -ea users $AUTHORITY_USER $AUTHORITY_PASSWORD"
 
 for x in ${TESTS[@]};
 do
     COMMAND="$COMMAND_RUN emrt.necd.test.$x $COMMAND_ARGS";
-    echo "Running: $COMMAND"
     bash -c "$COMMAND"
 done
